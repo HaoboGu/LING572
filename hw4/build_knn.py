afaebf633ec812e86372c96589da2bd7ecd17d63
HaoboGu <haobogu@outlook.com>
2018-02-01 19:01:00 -0800

    Test version of knn

--- hw4/build_knn.py
+++ hw4/build_knn.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.spatial.distance import cdist
 import time
 import os
+from collections import Counter
+from operator import itemgetter
 
 
 def read_data(filename):
@@ -71,15 +73,163 @@ def read_test_data(filename, features_set):
     return data.transpose()
 
 
+def knn(train, test, k, distance_func):
+    """
+    Knn algorithm.
+    :param train: np.array([label, feature_dict]]
+    :param test: np.array([label, feature_dict]]
+    :param k: int
+    :param distance_func: 1 means using euclidean distance in knn, 2 means cosine distance
+    :type distance_func: int
+    :return:
+    """
+
+    # create lists used in distance calculation
+    training_labels = train[0]
+    training_lists = train[1]
+    test_labels = test[0]
+    test_lists = test[1]
+    label_set = set(training_labels)
+    # calculate distance matrix
+    if distance_func == 1:
+        matrix = cdist(np.array(test_lists), np.array(training_lists), metric='euclidean')
+    elif distance_func == 2:
+        matrix = cdist(np.array(test_lists), np.array(training_lists), metric='cosine')
+    else:
+        return
+
+    result_label = []
+    out_strings = []
+
+    for i in range(matrix.shape[0]):
+        # for each test instance
+        best_labels = training_labels[np.argpartition(matrix[i], k)[:k]]  # get top k similar labels
+        counts = dict(Counter(best_labels))  # count them
+        for l in label_set:  # add labels that aren't in top k
+            if l not in counts:
+                counts[l] = 0
+        denominator = sum(counts.values())
+        sorted_items = sorted(counts.items(), key=itemgetter(1), reverse=True)  # sort results by count
+        sorted_items = np.array(sorted_items)
+        # rearrange result by prob and then by label name
+        max_c = sorted_items[0][1]
+        max_s = sorted_items[0][0]
+        for index in range(len(sorted_items)):
+            c = sorted_items[index][1]
+            l = sorted_items[index][0]
+            if max_c == c and l < max_s:
+                sorted_items[index][0], sorted_items[0][0] = sorted_items[0][0], sorted_items[index][0]
+                sorted_items[index][1], sorted_items[0][1] = sorted_items[0][1], sorted_items[index][1]
+                max_s = sorted_items[0][0]
+            elif max_c == c:
+                continue
+            else:
+                break
+
+        result_label.append(sorted_items[0][0])  # add best label to result
+
+        out_string = 'array:' + str(i) + ' ' + test_labels[i]
+        for key, value in sorted_items:
+            out_string += ' ' + str(key) + ' ' + ("%.5f" % (float(value)/denominator))
+        out_string += '\n'
+        out_strings.append(out_string)
+
+    return out_strings, result_label
+
+
+def print_confusion_matrix(train_result, train_truth, test_result, test_truth):
+    print('Confusion matrix for the training data:\nrow is the truth, column is the system output\n')
+    label_set = list(set(train_truth))
+    dimension = len(label_set)
+    matrix = np.zeros([dimension, dimension])
+    map_to_num = {}
+    map_to_l = {}
+    index = 0
+    yes_train = 0
+    for item in label_set:
+        map_to_num[item] = index
+        map_to_l[index] = item
+        index += 1
+    for i in range(len(train_truth)):
+        if train_result[i] == train_truth[i]:
+            col = map_to_num[train_result[i]]
+            row = map_to_num[train_truth[i]]
+            matrix[row][col] += 1
+            yes_train += 1
+        else:
+            col = map_to_num[train_result[i]]
+            row = map_to_num[train_truth[i]]
+            matrix[row][col] += 1
+
+    print_str = '            '
+    for i in range(len(label_set)):
+        print_str += ' ' + map_to_l[i]  # first row
+    print_str += '\n'
+    for i in range(len(label_set)):
+        print_str += map_to_l[i]  # first col
+        for j in range(len(label_set)):
+            print_str += ' ' + str(int(matrix[i][j]))
+        print_str += '\n'
+    print_str += '\n Training accuracy=' + ("%.5f" % (yes_train/len(train_result))) + '\n'
+    print(print_str)
+
+    # print test result
+    print('Confusion matrix for the test data:\nrow is the truth, column is the system output\n')
+    label_set = list(set(test_truth))
+    dimension = len(label_set)
+    matrix = np.zeros([dimension, dimension])
+    map_to_num = {}
+    map_to_l = {}
+    index = 0
+    yes_test = 0
+    for item in label_set:
+        map_to_num[item] = index
+        map_to_l[index] = item
+        index += 1
+    for i in range(len(test_truth)):
+        if test_result[i] == test_truth[i]:
+            col = map_to_num[test_result[i]]
+            row = map_to_num[test_truth[i]]
+            matrix[row][col] += 1
+            yes_test += 1
+        else:
+            col = map_to_num[test_result[i]]
+            row = map_to_num[test_truth[i]]
+            matrix[row][col] += 1
+
+    print_str = '            '
+    for i in range(len(label_set)):
+        print_str += ' ' + map_to_l[i]  # first row
+    print_str += '\n'
+    for i in range(len(label_set)):
+        print_str += map_to_l[i]  # first col
+        for j in range(len(label_set)):
+            print_str += ' ' + str(int(matrix[i][j]))
+        print_str += '\n'
+    print_str += '\n testing accuracy=' + ("%.5f" % (yes_test/len(test_result)))+ '\n'
+    print(print_str)
+
+
+def write_system_output(training_result, test_result, output_filename):
+    f = open(output_filename, 'w')
+    f.write("%%%%% training data:\n")
+    for o_str in training_result[0]:
+        f.write(o_str)
+    f.write("\n\n%%%%% test data:\n")
+    for o_str in test_result[0]:
+        f.write(o_str)
+    f.close()
+
+
 if __name__ == "__main__":
-    use_local_file = True
+    use_local_file = False
     if use_local_file:
         if 'hw4' in os.listdir():
             os.chdir('hw4')
         training_data_filename = 'examples/train.vectors.txt'
         test_data_filename = 'examples/test.vectors.txt'
         k_val = 5
-        similarity_func = 1
+        similarity_func = 2
         sys_output = 'sys.out'
     else:
         training_data_filename = sys.argv[1]
@@ -92,54 +242,35 @@ if __name__ == "__main__":
     training_data, word_set = read_data(training_data_filename)
     test_data = read_test_data(test_data_filename, word_set)
     word_list = list(word_set)
-    training_lists = []
-    training_labels = []
-    train_word_list = []
-    for train_label, train_d in training_data:
+
+    # rearrange data based on order in word list
+    train_instances = []
+    train_labels = []
+    for label, d in training_data:
         train_list = []
         for i in range(len(word_list)):
-            train_list.append(train_d[word_list[i]])
-            train_word_list.append(word_list[i])
-        training_lists.append(train_list)
-        training_labels.append(train_label)
+            train_list.append(d[word_list[i]])
+        train_instances.append(train_list)
+        train_labels.append(label)
+    train_labels = np.array(train_labels)
 
-
-    test_lists = []
-    test_labels = []
-    test_word_list = []
-    for test_label, test_d in test_data:
+    test_instances = []
+    test_truth = []
+    for label, d in test_data:
         test_list = []
         for i in range(len(word_list)):
-            test_list.append(test_d[word_list[i]])
-            test_word_list.append(word_list[i])
-        test_lists.append(test_list)
-        test_labels.append(test_label)
-    training_labels = np.array(training_labels)
-    correct, wrong = 0, 0
-    train_matrix = cdist(np.array(training_lists), np.array(training_lists), metric='euclidean')
-    for i in range(train_matrix.shape[0]):
-        best_labels = training_labels[np.argpartition(train_matrix[i], k_val)[:k_val]]
-        unique, pos = np.unique(best_labels, return_inverse=True)
-        counts = np.bincount(pos)
-        maxpos = counts.argmax()
-        if training_labels[i] == unique[maxpos]:
-            correct += 1
-        else:
-            wrong += 1
+            test_list.append(d[word_list[i]])
+        test_instances.append(test_list)
+        test_truth.append(label)
 
-    matrix = cdist(np.array(test_lists), np.array(training_lists), metric='cosine')
+    result_test = knn([train_labels, train_instances], [test_truth, test_instances], k_val, similarity_func)
+    result_train = knn([train_labels, train_instances], [train_labels, train_instances], k_val, similarity_func)
 
-    correct, wrong = 0, 0
-    for i in range(matrix.shape[0]):
-        best_labels = training_labels[np.argpartition(matrix[i], k_val)[:k_val]]
-        unique, pos = np.unique(best_labels, return_inverse=True)
-        counts = np.bincount(pos)
-        maxpos = counts.argmax()
-        if training_labels[i] == unique[maxpos]:
-            correct += 1
-        else:
-            wrong += 1
-    print(wrong, correct)
+    # Write test results to sys_output
+    write_system_output(result_train, result_test, sys_output)
+    # Print confusion matrix to command line
+    print_confusion_matrix(result_train[1], train_labels, result_test[1], test_truth)
+
 
 
 
